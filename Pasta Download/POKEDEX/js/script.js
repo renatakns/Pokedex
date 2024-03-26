c5e2d4e47f181b390d56d6975b6721e247e409df
Renata Nascimento <knsrenat@gmail.com>
2024-03-26 20:34:07 -0300

    commit pokedex1

---
+++ Pasta Download/POKEDEX/js/script.js
@@ -0,0 +1,10 @@
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+const renderPokemon = async (pokemon) => {
+
+    const data = fetchPokemon(pokemon);
+    console.log(data);
+}
